e65710bf28054517aa7cf799f77945320a3d7cb4
James Hornung <hornung.james@gmail.com>
2016-07-10 16:12:11 -0500

    customer app functional

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -8,8 +8,51 @@ var connection = mysql.createConnection({
 	database: 'Bamazon_db'
 });
 
+//Establish Connection
 connection.connect(function(err){
 	if (err) throw err;
 	console.log('connected as id: ' + connection.threadId)
 });
+
+connection.query('SELECT * FROM products', function(err, res){
+if (err) throw err;
+console.log('');
+console.log('Items in Store')
+for(i=0;i<res.length;i++){
+	console.log('Item ID:' + res[i].id + ' Product Name: ' + res[i].ProductName + ' Price: ' + res[i].Price)
+}
+console.log('')
+placeOrder();
+})
+
+
+//FUNCTIONS
+//=============================================================================
+function placeOrder(){
+	inquirer.prompt([{
+		name: 'selectId',
+		message: 'Please enter the ID of the product you wish to purchase'
+	},{
+		name:'selectQuantity',
+		message: 'How many of this product would you like to order?'
+	}]).then(function(answer){
+	connection.query('SELECT * FROM products WHERE id = ?', [answer.selectId], function(err, res){
+		if(answer.selectQuantity > res[0].StockQuantity){
+			console.log('Insufficient Quantity');
+			console.log('This order has been cancelled');
+		}
+		else{
+			console.log('Thanks for your order');
+			console.log('You owe $' + res[0].Price * answer.selectQuantity);
+			connection.query('UPDATE products SET ? Where ?', [{
+				StockQuantity: res[0].StockQuantity - answer.selectQuantity
+			},{
+				id: answer.selectId
+			}], function(err, res){});
+		}
+	})
+
+}, function(err, res){})
+};
+
 
