79c3fe39ec357d01de537e5185420c93d819bef3
James Hornung <hornung.james@gmail.com>
2016-07-12 10:52:44 -0500

    improved validation on customer app

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -14,30 +14,48 @@ connection.connect(function(err){
 	console.log('connected as id: ' + connection.threadId)
 });
 
-connection.query('SELECT * FROM products', function(err, res){
-if (err) throw err;
-console.log('=================================================');
-console.log('Items in Store');
-console.log('=================================================');
-
-for(i=0;i<res.length;i++){
-	console.log('Item ID:' + res[i].id + ' Product Name: ' + res[i].ProductName + ' Price: ' + '$' + res[i].Price)
-}
-console.log('')
-console.log('=================================================');
-placeOrder();
-})
-
-
 //FUNCTIONS
 //=============================================================================
+
+//Displays all items available in store and then calls the place order function
+function showProducts(){
+	connection.query('SELECT * FROM products', function(err, res){
+		if (err) throw err;
+		console.log('=================================================');
+		console.log('Items in Store');
+		console.log('=================================================');
+
+		for(i=0;i<res.length;i++){
+			console.log('Item ID:' + res[i].id + ' Product Name: ' + res[i].ProductName + ' Price: ' + '$' + res[i].Price)
+		}
+		console.log('')
+		console.log('=================================================');
+		placeOrder();
+		})
+}
+
+//Prompts the user to place an order, fulfills the order, and then calls the new order function
 function placeOrder(){
 	inquirer.prompt([{
 		name: 'selectId',
-		message: 'Please enter the ID of the product you wish to purchase'
+		message: 'Please enter the ID of the product you wish to purchase',
+		validate: function(value){
+			var valid = value.match(/^[0-9]+$/)
+			if(valid){
+				return true
+			}
+				return 'Please enter a valid Product ID'
+		}
 	},{
 		name:'selectQuantity',
-		message: 'How many of this product would you like to order?'
+		message: 'How many of this product would you like to order?',
+		validate: function(value){
+			var valid = value.match(/^[0-9]+$/)
+			if(valid){
+				return true
+			}
+				return 'Please enter a numerical value'
+		}
 	}]).then(function(answer){
 	connection.query('SELECT * FROM products WHERE id = ?', [answer.selectId], function(err, res){
 		if(answer.selectQuantity > res[0].StockQuantity){
@@ -61,6 +79,7 @@ function placeOrder(){
 }, function(err, res){})
 };
 
+//Allows the user to place a new order or end the connection
 function newOrder(){
 	inquirer.prompt([{
 		type: 'confirm',
@@ -76,4 +95,7 @@ function newOrder(){
 	})
 };
 
+//Call the original function (all other functions are called recursively)
+//======================================================================
+showProducts();
 
