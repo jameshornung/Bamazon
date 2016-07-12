d97600a7e49115cb9fef65bb8998bf63c7c5211a
James Hornung <hornung.james@gmail.com>
2016-07-12 11:12:53 -0500

    improved validation to manager app

--- BamazonManager.js
+++ BamazonManager.js
@@ -8,13 +8,18 @@ var connection = mysql.createConnection({
 	database: 'Bamazon_db'
 });
 
-//Establish Connection
+//Establish Connection - Call the Manager Input function
 connection.connect(function(err){
 	if (err) throw err;
 	console.log('connected as id: ' + connection.threadId)
 	managerInput();
 });
 
+
+//FUNCTIONS
+//=============================================================
+
+//Prompt the user for the action they would like to perform and then call the new transaction function
 function managerInput(){
 	inquirer.prompt([{
 		type: 'list',
@@ -22,7 +27,6 @@ function managerInput(){
 		message: 'What would you like to do today?',
 		choices: ['1) View Products for sale', '2) View low inventory', '3) Add to inventory', '4) Add new product']
 	}]).then(function(answer){
-		console.log('answer selected', answer.input);
 		if(answer.input === '1) View Products for sale'){
 			connection.query('SELECT * FROM products', function(err, res){
 			if (err) throw err;
@@ -42,11 +46,12 @@ function managerInput(){
 		else if(answer.input === '2) View low inventory'){
 			connection.query('SELECT * FROM products WHERE StockQuantity < 5', function(err, res){
 				if (err) throw err;
+				console.log('')
 				for(i=0;i<res.length;i++){
 					console.log('Name: ' + res[i].ProductName);
 					console.log('Product ID: ' + res[i].id);
 					console.log('Quantity in Stock: ' + res[i].StockQuantity);
-					console.log('');
+					console.log('---------------------------');
 				}
 				newTransaction();
 			})
@@ -54,10 +59,24 @@ function managerInput(){
 		else if(answer.input === '3) Add to inventory'){
 			inquirer.prompt([{
 				name: 'item',
-				message: 'Enter the ID of the item you wish to update:'
+				message: 'Enter the ID of the item you wish to update:',
+				validate: function(value){
+					var valid = value.match(/^[0-9]+$/)
+					if(valid){
+						return true
+					}
+						return 'Please enter a numerical value'
+					}
 			},{
 				name: 'number',
-				message: 'How many items would you like to add to the current supply?'
+				message: 'How many items would you like to add to the current supply?',
+				validate: function(value){
+					var valid = value.match(/^[0-9]+$/)
+					if(valid){
+						return true
+					}
+						return 'Please enter a numerical value'
+					}
 			}]).then(function(answer){
 				connection.query('SELECT * FROM products WHERE id = ?', [answer.item], function(err, res){
 						connection.query('UPDATE products SET ? Where ?', [{
@@ -79,10 +98,24 @@ function managerInput(){
 				message: 'Enter a department for this product'
 			},{
 				name: 'price',
-				message: 'Enter a price for this product'
+				message: 'Enter a price for this product',
+				validate: function(value){
+					var valid = value.match(/^[0-9]+$/)
+					if(valid){
+						return true
+					}
+						return 'Please enter a numerical value'
+					}
 			},{
 				name: 'stock',
-				message: 'Please enter a stock quantity for this product'
+				message: 'Please enter a stock quantity for this product',
+				validate: function(value){
+					var valid = value.match(/^[0-9]+$/)
+					if(valid){
+						return true
+					}
+						return 'Please enter a numerical value'
+					}
 			}]).then(function(answer){
 				connection.query('INSERT into products SET ?', {
 					ProductName: answer.product,
@@ -97,21 +130,19 @@ function managerInput(){
 	})
 };
 
+//Prompt the user to see if they would like to perform another transaction or end the connection
 function newTransaction(){
 	inquirer.prompt([{
-		name: 'confirm',
-		message: 'Would you like to perform another transaction? (Y or N)'
+		type: 'confirm',
+		name: 'choice',
+		message: 'Would you like to perform another transaction?'
 	}]).then(function(answer){
-		if(answer.confirm.toUpperCase() === 'Y'){
+		if(answer.choice){
 			managerInput();
 		}
-		else if(answer.confirm.toUpperCase() === 'N'){
+		else{
 			console.log('Have a good day');
 			connection.end();
-		}
-		else{
-			console.log('Please make a valid selection (Y or N)');
-			newTransaction();
 		}
 	})
 }
